6f40e3e376b22aa6232111a4a215afc6184a74e3
willianbogarin <willianbogarin@hotmail.com>
2023-08-05 11:33:07 +0200

    Completed Flash Chat Project

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true 
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        
+        print(db)
         return true
     }
 
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Willian Bogarin Jr on 2023. 06. 03..
+//  Copyright © 2023. Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Message {
+    
+    let sender: String
+    let body: String
+}
